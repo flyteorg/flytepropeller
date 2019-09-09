5da8c445c3251c025c371d184625598299bd921b
Andrew Chan <achan@lyft.com>
2019-09-09 12:30:37 -0700

    Cache task executions to DataCatalog

---
+++ pkg/controller/catalog/config.go
@@ -0,0 +1,36 @@
+package catalog
+
+import (
+	"github.com/lyft/flytestdlib/config"
+)
+
+//go:generate pflags Config --default-var defaultConfig
+
+const ConfigSectionKey = "catalog-cache"
+
+var (
+	defaultConfig = &Config{
+		Type: NoOpDiscoveryType,
+	}
+
+	configSection = config.MustRegisterSection(ConfigSectionKey, defaultConfig)
+)
+
+type DiscoveryType = string
+
+const (
+	NoOpDiscoveryType   DiscoveryType = "noop"
+	LegacyDiscoveryType DiscoveryType = "legacy"
+	DataCatalogType     DiscoveryType = "datacatalog"
+)
+
+type Config struct {
+	Type     DiscoveryType `json:"type" pflag:"\"noop\", Catalog Implementation to use"`
+	Endpoint string        `json:"endpoint" pflag:"\"\", Endpoint for catalog service"`
+	Secure   bool          `json:"secure" pflag:"true, Connect with TSL/SSL"`
+}
+
+// Gets loaded config for Discovery
+func GetConfig() *Config {
+	return configSection.GetConfig().(*Config)
+}
