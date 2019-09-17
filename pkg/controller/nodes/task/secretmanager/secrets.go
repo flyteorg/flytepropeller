0c4d33825702802a09ea41f4970359cefd8331da
Ketan Umare <kumare@lyft.com>
2019-09-16 18:16:07 -0700

    Improved Plugin manager

--- pkg/controller/nodes/task/secretmanager/secrets.go
+++ pkg/controller/nodes/task/secretmanager/secrets.go
@@ -20,13 +20,12 @@ func (f FileEnvSecretManager) Get(ctx context.Context, key string) (string, erro
 	envVar := fmt.Sprintf("%s%s", f.envPrefix, key)
 	v, ok := os.LookupEnv(envVar)
 	if ok {
-		logger.Debugf(ctx, "Secret found %s", v)
 		return v, nil
 	}
 	secretFile := filepath.Join(f.secretPath, key)
 	if _, err := os.Stat(secretFile); err != nil {
 		if os.IsNotExist(err) {
-			return "", fmt.Errorf("secrets not found - file [%s], Env [%s]", secretFile, envVar)
+			return "", fmt.Errorf("secrets not found")
 		}
 		return "", err
 	}
