c399fc3e8e8c34249acade3ada010373361d4a38
Ketan Umare <kumare@lyft.com>
2019-09-16 18:16:07 -0700

    Improved Plugin manager

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/lyft/flyteplugins/go/tasks/array/k8s"
+	_ "github.com/lyft/flyteplugins/go/tasks/hive"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/container"
 
 	"github.com/lyft/flytepropeller/cmd/controller/cmd"
---
+++ pkg/controller/nodes/task/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"github.com/lyft/flytestdlib/config"
+	"k8s.io/apimachinery/pkg/util/sets"
+)
+
+//go:generate pflags Config --default-var defaultConfig
+
+const SectionKey = "tasks"
+
+var (
+	defaultConfig = &Config{
+		TaskPlugins: TaskPluginConfig{EnabledPlugins: []string{
+			"container",
+		}},
+		MaxPluginPhaseVersions: 1000,
+	}
+
+	section = config.MustRegisterSection(SectionKey, defaultConfig)
+)
+
+type Config struct {
+	TaskPlugins            TaskPluginConfig `json:"task-plugins" pflag:",Task plugin configuration"`
+	MaxPluginPhaseVersions uint32           `json:"max-plugin-phase-versions" pflag:",Maximum number of plugin phase versions allowed for one phase."`
+}
+
+type TaskPluginConfig struct {
+	EnabledPlugins []string
+}
+
+func (p TaskPluginConfig) GetEnabledPluginsSet() sets.String {
+	return sets.NewString(p.EnabledPlugins...)
+}
+
+func GetConfig() *Config {
+	return section.GetConfig().(*Config)
+}
