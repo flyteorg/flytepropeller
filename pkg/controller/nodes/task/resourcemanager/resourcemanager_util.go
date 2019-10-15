cb778f1a1073844eb2f7dbd024736bf8e2e1c171
Chang-Hong Hsu <changhonghsu@lyft.com>
2019-10-15 14:36:42 -0700

    move GetResourceManagerByType to util

---
+++ pkg/controller/nodes/task/resourcemanager/resourcemanager_util.go
@@ -0,0 +1,29 @@
+package resourcemanager
+
+import (
+	"context"
+	rmConfig "github.com/lyft/flytepropeller/pkg/controller/nodes/task/resourcemanager/config"
+	"github.com/lyft/flytestdlib/logger"
+	"github.com/lyft/flytestdlib/promutils"
+)
+
+func GetResourceManagerByType(ctx context.Context, managerType Type, scope promutils.Scope) (
+	Factory, error) {
+
+	switch managerType {
+	case TypeNoop:
+		logger.Infof(ctx, "Using the NOOP resource manager")
+		return NoopResourceManager{}, nil
+	case TypeRedis:
+		logger.Infof(ctx, "Using Redis based resource manager")
+		config := rmConfig.GetResourceManagerConfig()
+		redisClient, err := NewRedisClient(ctx, config.RedisHostPath, config.RedisHostKey, config.RedisMaxRetries)
+		if err != nil {
+			logger.Errorf(ctx, "Unable to initialize a redis client for the resource manager: [%v]", err)
+			return nil, err
+		}
+		return NewRedisResourceManager(ctx, redisClient, scope.NewSubScope("flytepropeller:redisresourcemanager"))
+	}
+	logger.Infof(ctx, "Using the NOOP resource manager by default")
+	return NoopResourceManager{}, nil
+}
