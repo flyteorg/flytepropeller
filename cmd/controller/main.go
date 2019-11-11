9ed93776d881be77dff9cc16f2f268b1415e0d00
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2019-11-11 14:34:26 -0800

    Load spark and sidecar plugins

--- cmd/controller/main.go
+++ cmd/controller/main.go
@@ -5,6 +5,8 @@ import (
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/array/k8s"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/hive"
 	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/container"
+	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/sidecar"
+	_ "github.com/lyft/flyteplugins/go/tasks/plugins/k8s/spark"
 
 	"github.com/lyft/flytepropeller/cmd/controller/cmd"
 )
