1b433f804777644320238e97d3e4485e9d7e75c2
Surinder Singh <ssingh@lyft.com>
2020-03-01 09:04:38 -0800

    omitempty

--- pkg/apis/flyteworkflow/v1alpha1/node_status.go
+++ pkg/apis/flyteworkflow/v1alpha1/node_status.go
@@ -159,7 +159,7 @@ type NodeStatus struct {
 	DataDir              DataReference `json:"-"`
 	OutputDir            DataReference `json:"-"`
 	Attempts             uint32        `json:"attempts"`
-	SystemFailures       uint32        `json:"sf"`
+	SystemFailures       uint32        `json:"sf,omitempty"`
 	Cached               bool          `json:"cached"`
 
 	// This is useful only for branch nodes. If this is set, then it can be used to determine if execution can proceed
