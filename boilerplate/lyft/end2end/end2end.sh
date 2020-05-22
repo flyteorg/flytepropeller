c431ad4ba4c667fca2c534772d8698263ad02617
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-05-21 18:03:12 -0700

    Run end2end tests on PRs

---
+++ boilerplate/lyft/end2end/end2end.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# WARNING: THIS FILE IS MANAGED IN THE 'BOILERPLATE' REPO AND COPIED TO OTHER REPOSITORIES.
+# ONLY EDIT THIS FILE FROM WITHIN THE 'LYFT/BOILERPLATE' REPOSITORY:
+#
+# TO OPT OUT OF UPDATES, SEE https://github.com/lyft/boilerplate/blob/master/Readme.rst
+
+set -e
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+OUT="$(mktemp -d)"
+git clone git@github.com:lyft/flyte.git "${OUT}"
+
+pushd ${OUT}
+make kustomize
+make end2end
+popd
