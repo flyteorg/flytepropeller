c38b6b7c14cb5c67ff33707bcc41ef7075d7ec44
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-05-26 14:34:34 -0700

    wip

--- boilerplate/lyft/end2end/end2end.sh
+++ boilerplate/lyft/end2end/end2end.sh
@@ -13,8 +13,17 @@ OUT="${DIR}/tmp"
 rm -rf ${OUT}
 git clone -b ignore-log-errors https://github.com/lyft/flyte.git "${OUT}"
 
+# Create docker secret in flyte namespace
+kubectl create secret docker-registry githubpackages -n flyte \
+  --docker-server="docker.pkg.github.com" \
+  --docker-username="${DOCKER_USERNAME}" \
+  --docker-password="${DOCKER_PASSWORD}" \
+  --docker-email="localhost@localhost"
+
+kubectl patch serviceaccount default -n flyte -p '{"imagePullSecrets":[{"name":"githubpackages"}]}'
+
 pushd ${OUT}
-#sed -i.bak -e "s_docker.io/lyft/flytepropeller:v0.2.36_docker.pkg.github.com/${PROPELLER}_g" ${OUT}/kustomize/base/propeller/deployment.yaml
+sed -i.bak -e "s_docker.io/lyft/flytepropeller:v0.2.36_docker.pkg.github.com/${PROPELLER}_g" ${OUT}/kustomize/base/propeller/deployment.yaml
 make kustomize
 make end2end_execute
 popd
