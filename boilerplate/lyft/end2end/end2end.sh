97a9a46277f8fb08192f05def9284fdbe3c6f1b2
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-05-26 17:01:48 -0700

    add todos

--- boilerplate/lyft/end2end/end2end.sh
+++ boilerplate/lyft/end2end/end2end.sh
@@ -11,15 +11,18 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 OUT="${DIR}/tmp"
 rm -rf ${OUT}
-git clone -b ignore-log-errors https://github.com/lyft/flyte.git "${OUT}"
+git clone https://github.com/lyft/flyte.git "${OUT}"
 
+# TODO: load all images
 echo "Loading github docker images into 'kind' cluster to workaround this issue: https://github.com/containerd/containerd/issues/3291#issuecomment-631746985"
 docker login --username ${DOCKER_USERNAME} --password ${DOCKER_PASSWORD} docker.pkg.github.com
 docker pull docker.pkg.github.com/${PROPELLER}
 kind load docker-image docker.pkg.github.com/${PROPELLER}
 
 pushd ${OUT}
-sed -i.bak -e "s_docker.io/lyft/flytepropeller:v0.2.36_docker.pkg.github.com/${PROPELLER}_g" ${OUT}/kustomize/base/propeller/deployment.yaml
+# TODO: Only replace propeller if it's passed in
+# TODO: Support replacing other images too
+sed -i.bak -e "s_docker.io/lyft/flytepropeller:.*_docker.pkg.github.com/${PROPELLER}_g" ${OUT}/kustomize/base/propeller/deployment.yaml
 make kustomize
 make end2end_execute
 popd
