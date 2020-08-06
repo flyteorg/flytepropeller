5bc02d7f65c3b26bdd0772c527516910a677254b
Haytham AbuelFutuh <habuelfutuh@lyft.com>
2020-08-05 17:05:37 -0700

    Comments

--- pkg/controller/nodes/handler/state_test.go
+++ pkg/controller/nodes/handler/state_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+// A test to demonstrate how to unmarshal a serialized state from a workflow CRD.
 func TestDecodeTaskState(t *testing.T) {
 	str := `I/+DAwEBC1BsdWdpblN0YXRlAf+EAAEBAQVQaGFzZQEGAAAABf+EAQIA`
 	reader := base64.NewDecoder(base64.RawStdEncoding, bytes.NewReader([]byte(str)))
@@ -19,5 +20,6 @@ func TestDecodeTaskState(t *testing.T) {
 	err := dec.Decode(st)
 	if assert.NoError(t, err) {
 		t.Logf("Deserialized State: [%+v]", st)
+		assert.Equal(t, k8s.PluginPhaseStarted, st.Phase)
 	}
 }
