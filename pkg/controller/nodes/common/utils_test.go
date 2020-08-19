1afc28ba1edf4df47f4870b18955a3dda385904e
Anand Swaminathan <aswaminathan@lyft.com>
2020-08-18 22:05:25 -0700

    lint

--- pkg/controller/nodes/common/utils_test.go
+++ pkg/controller/nodes/common/utils_test.go
@@ -1,14 +1,15 @@
 package common
 
 import (
+	"testing"
+
 	"github.com/lyft/flytepropeller/pkg/apis/flyteworkflow/v1alpha1"
 	"github.com/stretchr/testify/assert"
-	"testing"
 )
 
 type ParentInfo struct {
 	uniqueID string
-	attempt uint32
+	attempt  uint32
 }
 
 func (p ParentInfo) GetUniqueID() v1alpha1.NodeID {
@@ -22,34 +23,33 @@ func (p ParentInfo) CurrentAttempt() uint32 {
 func TestGenerateUniqueID(t *testing.T) {
 	p := ParentInfo{
 		uniqueID: "u1",
-		attempt: uint32(2),
+		attempt:  uint32(2),
 	}
-	uniqueId, err := GenerateUniqueID(p, "n1")
+	uniqueID, err := GenerateUniqueID(p, "n1")
 	assert.Nil(t, err)
-	assert.Equal(t, "u1-2-n1", uniqueId)
+	assert.Equal(t, "u1-2-n1", uniqueID)
 }
 
 func TestGenerateUniqueIDLong(t *testing.T) {
 	p := ParentInfo{
 		uniqueID: "u1111111111323131231asdadasd",
-		attempt: uint32(2),
+		attempt:  uint32(2),
 	}
-	uniqueId, err := GenerateUniqueID(p, "n1")
+	uniqueID, err := GenerateUniqueID(p, "n1")
 	assert.Nil(t, err)
-	assert.Equal(t, "fraibbty", uniqueId)
+	assert.Equal(t, "fraibbty", uniqueID)
 }
 
 func TestCreateParentInfo(t *testing.T) {
 	gp := ParentInfo{
 		uniqueID: "u1",
-		attempt: uint32(2),
+		attempt:  uint32(2),
 	}
 	parent, err := CreateParentInfo(gp, "n1", uint32(1))
 	assert.Nil(t, err)
 	assert.Equal(t, "u1-2-n1", parent.GetUniqueID())
 	assert.Equal(t, uint32(1), parent.CurrentAttempt())
 }
-
 
 func TestCreateParentInfoNil(t *testing.T) {
 	parent, err := CreateParentInfo(nil, "n1", uint32(1))
