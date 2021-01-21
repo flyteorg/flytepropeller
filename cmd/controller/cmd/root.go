19c93b11a7ae9ab543a0b60cd9b8098a7b81d13b
Yuvraj <10830562+evalsocket@users.noreply.github.com>
2021-01-22 03:32:36 +0530

    Pass command-line arguments (#193)
    * Pass command line args
    
    Signed-off-by: Yuvraj <evalsocket@gmail.com>

--- cmd/controller/cmd/root.go
+++ cmd/controller/cmd/root.go
@@ -96,7 +96,7 @@ func init() {
 
 func initConfig(cmd *cobra.Command, _ []string) error {
 	configAccessor = viper.NewAccessor(config.Options{
-		StrictMode:  false,
+		StrictMode:  true,
 		SearchPaths: []string{cfgFile},
 	})
 	configAccessor.InitializePflags(cmd.Flags())
