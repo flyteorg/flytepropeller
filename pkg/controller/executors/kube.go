c5d60528a072070af24d2398748bda4c21b32795
Haytham Abuelfutuh <haytham@afutuh.com>
2021-03-05 16:50:15 -0800

    Replace NewClient with ClientBuilder in manager initialization
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- pkg/controller/executors/kube.go
+++ pkg/controller/executors/kube.go
@@ -3,6 +3,8 @@ package executors
 import (
 	"context"
 
+	"sigs.k8s.io/controller-runtime/pkg/cluster"
+
 	"sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -17,7 +19,7 @@ type Client interface {
 }
 
 type fallbackClientReader struct {
-	orderedClients []client.Client
+	orderedClients []client.Reader
 }
 
 func (c fallbackClientReader) Get(ctx context.Context, key client.ObjectKey, out client.Object) (err error) {
@@ -40,15 +42,34 @@ func (c fallbackClientReader) List(ctx context.Context, list client.ObjectList, 
 	return
 }
 
-// Creates a new k8s client that uses the cached client for reads and falls back to making API
-// calls if it failed. Write calls will always go to raw client directly.
-func NewFallbackClient(cachedClient, rawClient client.Client) (client.Client, error) {
+type FallbackClientBuilder struct {
+	uncached []client.Object
+}
+
+func (f *FallbackClientBuilder) WithUncached(objs ...client.Object) cluster.ClientBuilder {
+	f.uncached = append(f.uncached, objs...)
+	return f
+}
+
+func (f FallbackClientBuilder) Build(cache cache.Cache, config *interface{}, options client.Options) (client.Client, error) {
+	c, err := client.New(config, options)
+	if err != nil {
+		return nil, err
+	}
+
 	return client.NewDelegatingClient(client.NewDelegatingClientInput{
+		Client: c,
 		CacheReader: fallbackClientReader{
-			orderedClients: []client.Client{cachedClient, rawClient},
+			orderedClients: []client.Reader{cache, c},
 		},
-		Client: rawClient,
+		UncachedObjects: f.uncached,
 		// TODO figure out if this should be true?
 		// CacheUnstructured: true,
 	})
+}
+
+// Creates a new k8s client that uses the cached client for reads and falls back to making API
+// calls if it failed. Write calls will always go to raw client directly.
+func NewFallbackClientBuilder() *FallbackClientBuilder {
+	return &FallbackClientBuilder{}
 }
