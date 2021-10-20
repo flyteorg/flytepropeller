4d4a3da7e77aa4a4b9a6c4339eb7ad5ec7780088
Daniel Rammer <daniel@union.ai>
2021-10-20 12:01:53 -0500

    moved idFromMessage to separate func
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- events/admin_eventsink.go
+++ events/admin_eventsink.go
@@ -44,27 +44,13 @@ func (s *adminEventSink) Sink(ctx context.Context, message proto.Message) error 
 	logger.Debugf(ctx, "AdminEventSink received a new event %s", message.String())
 
 	// Short-circuit if event has already been sent
-	var idStr string
-	switch eventMessage := message.(type) {
-	case *event.WorkflowExecutionEvent:
-		wid := eventMessage.ExecutionId
-		idStr = fmt.Sprintf("%s:%s:%s:%d", wid.Project, wid.Domain, wid.Name, eventMessage.Phase)
-	case *event.NodeExecutionEvent:
-		nid := eventMessage.Id
-		wid := nid.ExecutionId
-		idStr = fmt.Sprintf("%s:%s:%s:%s:%d", wid.Project, wid.Domain, wid.Name, nid.NodeId, eventMessage.Phase)
-	case *event.TaskExecutionEvent:
-		tid := eventMessage.TaskId
-		nid := eventMessage.ParentNodeExecutionId
-		wid := nid.ExecutionId
-		idStr = fmt.Sprintf("%s:%s:%s:%s:%s:%s:%s:%s:%d:%d", tid.Project, tid.Domain, tid.Name, tid.Version, wid.Project, wid.Domain, wid.Name, nid.NodeId, eventMessage.Phase, eventMessage.PhaseVersion)
-	default:
-		return fmt.Errorf("unknown event type [%s]", eventMessage.String())
+	id, err := idFromMessage(message)
+	if err != nil {
+		return fmt.Errorf("Failed to parse message id [%v]", message.String())
 	}
 
-	id := []byte(idStr)
 	if s.filter.Contains(ctx, id) {
-		logger.Debugf(ctx, "event '%s' has already been sent", idStr)
+		logger.Debugf(ctx, "event '%s' has already been sent", string(id))
 		return nil
 	}
 
@@ -115,6 +101,28 @@ func (s *adminEventSink) Close() error {
 	return nil
 }
 
+func idFromMessage(message proto.Message) ([]byte, error) {
+	var id string
+	switch eventMessage := message.(type) {
+	case *event.WorkflowExecutionEvent:
+		wid := eventMessage.ExecutionId
+		id = fmt.Sprintf("%s:%s:%s:%d", wid.Project, wid.Domain, wid.Name, eventMessage.Phase)
+	case *event.NodeExecutionEvent:
+		nid := eventMessage.Id
+		wid := nid.ExecutionId
+		id = fmt.Sprintf("%s:%s:%s:%s:%d", wid.Project, wid.Domain, wid.Name, nid.NodeId, eventMessage.Phase)
+	case *event.TaskExecutionEvent:
+		tid := eventMessage.TaskId
+		nid := eventMessage.ParentNodeExecutionId
+		wid := nid.ExecutionId
+		id = fmt.Sprintf("%s:%s:%s:%s:%s:%s:%s:%s:%d:%d", tid.Project, tid.Domain, tid.Name, tid.Version, wid.Project, wid.Domain, wid.Name, nid.NodeId, eventMessage.Phase, eventMessage.PhaseVersion)
+	default:
+		return nil, fmt.Errorf("unknown event type [%s]", eventMessage.String())
+	}
+
+	return []byte(id), nil
+}
+
 func initializeAdminClientFromConfig(ctx context.Context) (client service.AdminServiceClient, err error) {
 	cfg := admin2.GetConfig(ctx)
 	clients, err := admin2.NewClientsetBuilder().WithConfig(cfg).Build(ctx)
@@ -137,7 +145,7 @@ func ConstructEventSink(ctx context.Context, config *Config, scope promutils.Sco
 			return nil, err
 		}
 
-		filter, err := fastcheck.NewOppoBloomFilter(20000, scope.NewSubScope("admin_filter"))
+		filter, err := fastcheck.NewOppoBloomFilter(50000, scope.NewSubScope("admin_filter"))
 		if err != nil {
 			return nil, err
 		}
