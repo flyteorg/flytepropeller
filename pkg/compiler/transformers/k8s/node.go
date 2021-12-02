9e632487b6ea9a8dcd7cc0a8956d4df76647f85f
Kenny Workman <kennyworkman@sbcglobal.net>
2021-12-01 21:49:18 -0800

    feat: strip annotation from node template
    Signed-off-by: Kenny Workman <kennyworkman@sbcglobal.net>

--- pkg/compiler/transformers/k8s/node.go
+++ pkg/compiler/transformers/k8s/node.go
@@ -221,6 +221,18 @@ func buildTasks(tasks []*core.CompiledTask, errs errors.CompileErrors) map[commo
 				errs.Collect(errors.NewValueCollisionError(taskID, "Id", taskID))
 			}
 
+			// We don't want Annotation data available at task runtime for performance.
+			if flyteTask.Template != nil &&
+				flyteTask.Template.Interface != nil &&
+				flyteTask.Template.Interface.Inputs != nil &&
+				flyteTask.Template.Interface.Inputs.Variables != nil {
+				for _, v := range flyteTask.Template.Interface.Inputs.Variables {
+					if v.Type != nil && v.Type.Annotation != nil {
+						v.Type.Annotation = nil
+					}
+				}
+			}
+
 			res[taskID] = &v1alpha1.TaskSpec{TaskTemplate: flyteTask.Template}
 		}
 	}
