c87a6759cca8c9427a692c74ff370f812ac1b30b
Haytham Abuelfutuh <haytham@afutuh.com>
2022-05-04 16:27:36 -0700

    Move code used only for testing to test file
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- pkg/compiler/validators/utils.go
+++ pkg/compiler/validators/utils.go
@@ -207,46 +207,6 @@ func LiteralTypeForLiteral(l *core.Literal) *core.LiteralType {
 	return nil
 }
 
-// LiteralToBinding converts a literal to a non-promise binding data.
-func LiteralToBinding(l *core.Literal) *core.BindingData {
-	switch l.GetValue().(type) {
-	case *core.Literal_Scalar:
-		return &core.BindingData{
-			Value: &core.BindingData_Scalar{
-				Scalar: l.GetScalar(),
-			},
-		}
-	case *core.Literal_Collection:
-		x := make([]*core.BindingData, 0, len(l.GetCollection().Literals))
-		for _, sub := range l.GetCollection().Literals {
-			x = append(x, LiteralToBinding(sub))
-		}
-
-		return &core.BindingData{
-			Value: &core.BindingData_Collection{
-				Collection: &core.BindingDataCollection{
-					Bindings: x,
-				},
-			},
-		}
-	case *core.Literal_Map:
-		x := make(map[string]*core.BindingData, len(l.GetMap().Literals))
-		for key, val := range l.GetMap().Literals {
-			x[key] = LiteralToBinding(val)
-		}
-
-		return &core.BindingData{
-			Value: &core.BindingData_Map{
-				Map: &core.BindingDataMap{
-					Bindings: x,
-				},
-			},
-		}
-	}
-
-	return nil
-}
-
 func GetTagForType(x *core.LiteralType) string {
 	if x.GetStructure() == nil {
 		return ""
