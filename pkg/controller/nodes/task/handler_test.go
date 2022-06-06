f0cc3526ab13044f88fbbc55db65f1991a3ad117
Kevin Su <pingsutw@apache.org>
2022-06-07 00:45:19 +0800

    Fix tests
    Signed-off-by: Kevin Su <pingsutw@apache.org>

--- pkg/controller/nodes/task/handler_test.go
+++ pkg/controller/nodes/task/handler_test.go
@@ -1123,6 +1123,8 @@ func Test_task_Handle_Reservation(t *testing.T) {
 			nCtx.OnNodeStateReader().Return(nr)
 			if tt.args.catalogFetch {
 				or := &ioMocks.OutputReader{}
+				deckPath := storage.DataReference("deck.html")
+				or.OnGetDeckPath().Return(&deckPath)
 				or.OnReadMatch(mock.Anything).Return(&core.LiteralMap{}, nil, nil)
 				c.OnGetMatch(mock.Anything, mock.Anything).Return(catalog.NewCatalogEntry(or, catalog.NewStatus(core.CatalogCacheStatus_CACHE_HIT, nil)), nil)
 			} else {
