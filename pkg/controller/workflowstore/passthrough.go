2ea8a820c04c8c9315b7388657e03c0e01f62119
Babis Kiosidis <babisk@spotify.com>
2022-07-26 13:20:32 +0300

    test happy offloaded spec scenario
    Signed-off-by: Babis Kiosidis <babisk@spotify.com>
    Signed-off-by: Babis Kiosidis <ckiosidis@gmail.com>

--- pkg/controller/workflowstore/passthrough.go
+++ pkg/controller/workflowstore/passthrough.go
@@ -80,8 +80,8 @@ func (p *passthroughWorkflowStore) Update(ctx context.Context, workflow *v1alpha
 	logger.Debugf(ctx, "Observed FlyteWorkflow Update (maybe finalizer)")
 	t := p.metrics.workflowUpdateLatency.Start()
 	if workflow.WorkflowStaticExecutionObj != "" {
-		workflow.SubWorkflows = make(map[v1alpha1.WorkflowID]*v1alpha1.WorkflowSpec)
-		workflow.Tasks = make(map[v1alpha1.TaskID]*v1alpha1.TaskSpec)
+		workflow.SubWorkflows = nil
+		workflow.Tasks = nil
 		workflow.WorkflowSpec = nil
 	}
 	newWF, err = p.wfClientSet.FlyteWorkflows(workflow.Namespace).Update(ctx, workflow, v1.UpdateOptions{})
