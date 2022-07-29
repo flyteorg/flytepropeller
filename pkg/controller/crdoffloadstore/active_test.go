02b1946b8c8193bf0520f524a2ff5a8d66e0b3ab
Daniel Rammer <daniel@union.ai>
2022-07-29 10:00:50 -0500

    added activeCRDOffloadStore unit tests
    Signed-off-by: Daniel Rammer <daniel@union.ai>
    Signed-off-by: Babis Kiosidis <ckiosidis@gmail.com>

--- pkg/controller/crdoffloadstore/active_test.go
+++ pkg/controller/crdoffloadstore/active_test.go
@@ -15,7 +15,7 @@ import (
 	mock "github.com/stretchr/testify/mock"
 )
 
-func TestLruCRDOffloadStore(t *testing.T) {
+func TestActiveCRDOffloadStore(t *testing.T) {
 	ctx := context.TODO()
 	staticWorkflowData := v1alpha1.StaticWorkflowData{}
 
@@ -25,17 +25,16 @@ func TestLruCRDOffloadStore(t *testing.T) {
 		mockStore.OnGetMatch(mock.Anything, mock.Anything).Return(&staticWorkflowData, nil)
 
 		scope := promutils.NewTestScope()
-		lruStore, err := NewLRUCRDOffloadStore(mockStore, 1, scope)
-		assert.NoError(t, err)
+		activeStore := NewActiveCRDOffloadStore(mockStore, scope)
 
 		// Get from underlying CRDOffloadStore
-		data, err := lruStore.Get(ctx, "foo")
+		data, err := activeStore.Get(ctx, "foo")
 		assert.NoError(t, err)
 		assert.True(t, reflect.DeepEqual(staticWorkflowData, *data))
 		mockStore.AssertNumberOfCalls(t, "Get", 1)
 
 		// Get from cache
-		data, err = lruStore.Get(ctx, "foo")
+		data, err = activeStore.Get(ctx, "foo")
 		assert.NoError(t, err)
 		assert.True(t, reflect.DeepEqual(staticWorkflowData, *data))
 		mockStore.AssertNumberOfCalls(t, "Get", 1)
@@ -48,53 +47,24 @@ func TestLruCRDOffloadStore(t *testing.T) {
 		mockStore.OnRemoveMatch(mock.Anything, mock.Anything).Return(nil)
 
 		scope := promutils.NewTestScope()
-		lruStore, err := NewLRUCRDOffloadStore(mockStore, 1, scope)
-		assert.NoError(t, err)
+		activeStore := NewActiveCRDOffloadStore(mockStore, scope)
 
 		// Get from underlying CRDOffloadStore
-		data, err := lruStore.Get(ctx, "foo")
+		data, err := activeStore.Get(ctx, "foo")
 		assert.NoError(t, err)
 		assert.True(t, reflect.DeepEqual(staticWorkflowData, *data))
 		mockStore.AssertNumberOfCalls(t, "Get", 1)
 
 		// Remove from underlying CRDOffloadStore
-		err = lruStore.Remove(ctx, "foo")
+		err = activeStore.Remove(ctx, "foo")
 		assert.NoError(t, err)
 		mockStore.AssertNumberOfCalls(t, "Remove", 1)
 
 		// Get from cache
-		data, err = lruStore.Get(ctx, "foo")
+		data, err = activeStore.Get(ctx, "foo")
 		assert.NoError(t, err)
 		assert.True(t, reflect.DeepEqual(staticWorkflowData, *data))
 		mockStore.AssertNumberOfCalls(t, "Get", 2)
-	})
-
-	t.Run("Eviction", func(t *testing.T) {
-		// initialize mocks
-		mockStore := &mocks.CRDOffloadStore{}
-		mockStore.OnGetMatch(mock.Anything, mock.Anything).Return(&staticWorkflowData, nil)
-
-		scope := promutils.NewTestScope()
-		lruStore, err := NewLRUCRDOffloadStore(mockStore, 1, scope)
-		assert.NoError(t, err)
-
-		// Get from underlying CRDOffloadStore
-		data, err := lruStore.Get(ctx, "foo")
-		assert.NoError(t, err)
-		assert.True(t, reflect.DeepEqual(staticWorkflowData, *data))
-		mockStore.AssertNumberOfCalls(t, "Get", 1)
-
-		// Get from cache
-		data, err = lruStore.Get(ctx, "bar")
-		assert.NoError(t, err)
-		assert.True(t, reflect.DeepEqual(staticWorkflowData, *data))
-		mockStore.AssertNumberOfCalls(t, "Get", 2)
-
-		// Get eviction
-		data, err = lruStore.Get(ctx, "foo")
-		assert.NoError(t, err)
-		assert.True(t, reflect.DeepEqual(staticWorkflowData, *data))
-		mockStore.AssertNumberOfCalls(t, "Get", 3)
 	})
 
 	t.Run("UnderlyingError", func(t *testing.T) {
@@ -103,11 +73,10 @@ func TestLruCRDOffloadStore(t *testing.T) {
 		mockStore.OnGetMatch(mock.Anything, mock.Anything).Return(nil, fmt.Errorf("foo"))
 
 		scope := promutils.NewTestScope()
-		lruStore, err := NewLRUCRDOffloadStore(mockStore, 1, scope)
-		assert.NoError(t, err)
+		activeStore := NewActiveCRDOffloadStore(mockStore, scope)
 
 		// Get from underlying CRDOffloadStore
-		data, err := lruStore.Get(ctx, "foo")
+		data, err := activeStore.Get(ctx, "foo")
 		assert.Error(t, err)
 		assert.Nil(t, data)
 		mockStore.AssertNumberOfCalls(t, "Get", 1)
