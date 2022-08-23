7eb032764d9f164ee9e21185921c2e80465f0140
Haytham Abuelfutuh <haytham@afutuh.com>
2022-08-23 14:11:46 -0700

    Use fnv64 to create a hash for child workflow executions
    Signed-off-by: Haytham Abuelfutuh <haytham@afutuh.com>

--- pkg/controller/nodes/subworkflow/util.go
+++ pkg/controller/nodes/subworkflow/util.go
@@ -11,10 +11,11 @@ import (
 const maxLengthForSubWorkflow = 20
 
 func GetChildWorkflowExecutionID(nodeExecID *core.NodeExecutionIdentifier, attempt uint32) (*core.WorkflowExecutionIdentifier, error) {
-	name, err := encoding.FixedLengthUniqueIDForParts(maxLengthForSubWorkflow, nodeExecID.ExecutionId.Name, nodeExecID.NodeId, strconv.Itoa(int(attempt)))
+	name, err := encoding.FixedLengthUniqueIDForParts(maxLengthForSubWorkflow, []string{nodeExecID.ExecutionId.Name, nodeExecID.NodeId, strconv.Itoa(int(attempt))})
 	if err != nil {
 		return nil, err
 	}
+
 	// Restriction on name is 20 chars
 	return &core.WorkflowExecutionIdentifier{
 		Project: nodeExecID.ExecutionId.Project,
@@ -24,7 +25,8 @@ func GetChildWorkflowExecutionID(nodeExecID *core.NodeExecutionIdentifier, attem
 }
 
 func GetChildWorkflowExecutionIDV2(nodeExecID *core.NodeExecutionIdentifier, attempt uint32) (*core.WorkflowExecutionIdentifier, error) {
-	name, err := encoding.FixedLengthUniqueIDForParts(maxLengthForSubWorkflow, nodeExecID.ExecutionId.Name, nodeExecID.NodeId, strconv.Itoa(int(attempt)))
+	name, err := encoding.FixedLengthUniqueIDForParts(maxLengthForSubWorkflow, []string{nodeExecID.ExecutionId.Name, nodeExecID.NodeId, strconv.Itoa(int(attempt))},
+		encoding.NewAlgorithmOption(encoding.Algorithm64))
 	if err != nil {
 		return nil, err
 	}
@@ -33,15 +35,6 @@ func GetChildWorkflowExecutionIDV2(nodeExecID *core.NodeExecutionIdentifier, att
 	return &core.WorkflowExecutionIdentifier{
 		Project: nodeExecID.ExecutionId.Project,
 		Domain:  nodeExecID.ExecutionId.Domain,
-		Name:    EnsureExecIDWithinLength(nodeExecID.ExecutionId.Name, name, maxLengthForSubWorkflow),
+		Name:    name,
 	}, nil
-}
-
-func EnsureExecIDWithinLength(execID, subName string, maxLength int) string {
-	maxLengthRemaining := maxLength - len(subName)
-	if len(execID) < maxLengthRemaining {
-		return execID + subName
-	}
-
-	return execID[:maxLengthRemaining] + subName
 }
