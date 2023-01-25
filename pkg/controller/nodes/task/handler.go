2f3e97f7533c9596dabb4731afe6456d407f345a
Daniel Rammer <daniel@union.ai>
2023-01-24 20:20:21 -0600

    only bypassing abort on success rather than all terminal phases
    Signed-off-by: Daniel Rammer <daniel@union.ai>

--- pkg/controller/nodes/task/handler.go
+++ pkg/controller/nodes/task/handler.go
@@ -770,7 +770,7 @@ func (t Handler) Abort(ctx context.Context, nCtx handler.NodeExecutionContext, r
 	currentPhase := nCtx.NodeStateReader().GetTaskNodeState().PluginPhase
 	logger.Debugf(ctx, "Abort invoked with phase [%v]", currentPhase)
 
-	if currentPhase.IsTerminal() {
+	if currentPhase.IsSuccess() {
 		logger.Debugf(ctx, "Returning immediately from Abort since task is already in terminal phase.", currentPhase)
 		return nil
 	}
